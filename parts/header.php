5835cced565b043cbc7877dc2b627bfa043ac803
franhaselden <fran@haselden.co.uk>
2015-02-24 15:12:04 +0000

    Initialise parts

---
+++ parts/header.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>My Knitting Bag</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+</head>
+
+<body>
